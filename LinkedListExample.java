44f48f01571664bda4cca6a5b78f2e3db57c27ea
Swati-Tech09 <80823399+Swati-Tech09@users.noreply.github.com>
2023-10-01 00:41:56 +0530

    Add LinkedList

---
+++ LinkedListExample.java
@@ -0,0 +1,23 @@
+import java.util.LinkedList;
+
+
+// LinkedList can use as Stack with push - LIFO
+// offer can work as queue -FIFO
+public class LinkedListExample {
+    public static void main(String[] args) {
+
+        LinkedList<String> l = new LinkedList<>();
+
+        l.push("A");
+        l.push("B");
+        l.push("C");
+
+        System.out.println(l.poll());
+
+        l.offer("A");
+        l.offer("B");
+        l.offer("C");
+        System.out.println(l.poll());
+
+    }
+}
